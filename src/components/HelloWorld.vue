6580b9db55d075d57091c0375002dab6089301f8
Frank-Chan <gisfeifly@gmail.com>
2024-06-20 19:00:09 +0800

    Feat:添加标记点和wgs84->gcj02Geo的转换标记示例

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,15 +13,15 @@
 <script>
 import "ol/ol.css";
 import { Map, View } from "ol";
-// 图层
-import { Tile } from "ol/layer";
 // openLayers自带的数据源
-import OSM from "ol/source/OSM";
 import TileLayer from "ol/layer/Tile";
 import { TileDebug } from "ol/source";
+import { transform } from "ol/proj";
+
 import GeoUtil from "@/js/geoUtil";
-import { gcj02Mercator, transformToGCJ02Mecator } from "@/js/gcj02";
+import { gcj02Geo, gcj02Mercator, transformToGCJ02Mecator } from "@/js/gcj02";
 import MapSwitcher from "./MapSwitcher.vue";
+import VectorLayer from "@/js/layer/VectorLayer";
 
 export default {
   name: "olMap",
@@ -37,6 +37,7 @@ export default {
         lon: 114.07,
         lat: 22.53,
       },
+      map: null,
     };
   },
   mounted() {
@@ -44,15 +45,6 @@ export default {
   },
   methods: {
     initMap() {
-      let tileOSM = new Tile({
-        // ol自带的数据源
-        source: new OSM(),
-        // 如果要引入其他地图的数据源，通过XYZ引入
-        // source: new XYZ({
-        //   url: "http://t3.tianditu.com/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=你的密钥，",
-        // }),
-      });
-
       let view = new View({
         zoom: this.zoom,
         /* center: [this.center.lon, this.center.lat],        
@@ -63,11 +55,11 @@ export default {
       });
       // 建议将map实例放在全局变量中，如果放在data中，数据量太大会造成ol卡死
       let map = new Map({
-        layers: [tileOSM],
         view,
         target: "olMap",
       });
       window.map = map;
+      this.map = map;
 
       //添加虚拟网格瓦片图层
       const debugLayer = new TileLayer({
@@ -76,8 +68,33 @@ export default {
       });
       map.addLayer(debugLayer);
 
-      map.baseMapLayer = tileOSM;
       GeoUtil.setBaseMap("GDVecTile"); //设置底图为高德矢量切片底图
+      // GeoUtil.setBaseMap("OSM"); //设置底图为OSM
+
+      this.addMarkPoint();
+    },
+    /**
+     * 添加标记点
+     */
+    addMarkPoint() {
+      //添加2个高德坐标系的点(内绿外红的圆点)
+      const vectorLayer = new VectorLayer();
+      this.map.addLayer(vectorLayer);
+      const pointStyle = {
+        circleRadius: 10,
+        fill: "#00F",
+        circleStrokeColor: "#F00",
+        circleStrokeWidth: 8,
+      };
+      vectorLayer.addPoint([114.062022, 22.54287], pointStyle); // 广东省深圳市福田区莲花街道深圳市人民政府深圳博物馆，东南角
+      vectorLayer.addPoint([114.057249, 22.54419], pointStyle); //广东省深圳市福田区莲花街道福中路286号深圳市民中心，在市政府西北角
+      //const wgs84Point = [114.05491187349624, 22.546044790720085];//市民中心，深圳市统计局天地图影像图锚点
+      //#region wgs84坐标转gcj02Geo坐标后展示
+      const wgs84Point = [103.85072079999999, 1.2898543999999998]; //新加坡高等法院wgs84坐标点
+      const gcj02GeoPoint = transform(wgs84Point, "EPSG:4326", gcj02Geo); //WGS84转高德地理坐标系
+      console.log("转换结果：", gcj02GeoPoint);
+      vectorLayer.addPoint(gcj02GeoPoint, pointStyle); 
+      //#endregion
     },
   },
 };
